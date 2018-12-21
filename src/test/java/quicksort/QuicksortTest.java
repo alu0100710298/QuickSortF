abcf2df138de3c489d0e665e43af85fa01450816
Sergio Moreno Martin <alu0100710298@ull.edu.es>
2018-12-21 02:24:48 +0000

    formateando QuicksortTest.java

--- src/test/java/quicksort/QuicksortTest.java
+++ src/test/java/quicksort/QuicksortTest.java
@@ -8,13 +8,11 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class QuicksortTest {
-    @Test
+	@Test
 
 	public void testMain() {
 
-		
-
-		int [] input = new int[]{6,1,7,9,3,8,2,5,4,0};
+		int[] input = new int[] { 6, 1, 7, 9, 3, 8, 2, 5, 4, 0 };
 
 		Quicksort sort = new Quicksort();
 
@@ -22,23 +20,9 @@ public class QuicksortTest {
 
 		String result = new String();
 
-	
-
-		for(int i:input)
+		for (int i : input)
 
 			result = result + " " + i;
-
-		
-
-		 /**
-
-         * Comprobamos que la lista desordenada que le pasamos da como salida 
-
-         * la lista ordenada, cumpliendo el formato que se le pasa como string. 
-
-         * 
-
-         */ 
 
 		assertEquals(" 0 1 2 3 4 5 6 7 8 9", result);
 
